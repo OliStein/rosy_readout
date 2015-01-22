16a3d1f24170944b98b3521c1049e9bf4fa57cd3
Oliver Stein <oliver.stein@cern.ch>
2015-01-22 15:18:45 +0100

    including main.py, main script which when envoced will call all other routines

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 22, 2015
+@author: Oliver Stein
+mail: oliver.stein@cern.ch
+'''
+
+
+'''
+Main script for connecting and communincating with Cividec ROSY box
+
+'''
+
+# Default Packages
+
+import sys
+import os
+import numpy as np
+
+#Path
+home =  os.getcwd()
+
+sys.path.append(os.path.join(home,'sub_scripts'))
+
+
+
+# Custom Packages
